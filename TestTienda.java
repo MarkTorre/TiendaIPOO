322c69bdc9d781ca7aaa70e71f3cbe4ffa548090
Serysev <62380919+Serysev@users.noreply.github.com>
2020-03-24 17:15:33 +0100

    Ahora la tienda se ve cuando inicias el programa

--- TestTienda.java
+++ TestTienda.java
@@ -11,26 +11,23 @@ public class TestTienda {
         int code;
 
         while (seguir == true) {
-            System.out.println("Quiere introducir un Componente? Si/NO");
+            System.out.println("Quiere añadir un componente? si/no");
             decide = scan.next();
             if (decide.equals("SI") == true || decide.equals("si") == true) {
-                System.out.println("Introduzca el codigo del componente que desea introducir");
+                System.out.println("Introduzca el código del componente que desea añadir");
                 code = scan.nextInt();
                 if(!miCarrito.anyadirProducto(code, tienda)){                                                           
-                    System.out.println("\n\tEl codigo que ha introducido no existe.\n");
+                    System.out.println("\n\tEl código que ha introducido no existe.\n");
                 }
-
 
             } else if (decide.equals("NO") == true || decide.equals("no") == true) {
                 System.out.println("\n\nCesta de compra actual. Usted tiene:\n" + miCarrito.toString());
-
                 seguir = false;
-
             }
         }
-
     }
 
+    
     public static void retirarProducto(CarritoDeLaCompra miCarrito) {
         Scanner scan = new Scanner(System.in);
         boolean seguir = true;
@@ -38,17 +35,17 @@ public class TestTienda {
         int code;
 
         while (seguir == true) {
-            System.out.println("Quiere retirar un Componente? Si/NO");
+            System.out.println("Quiere retirar un componente? si/no");
             decide = scan.next();
             if (decide.equals("SI") == true || decide.equals("si") == true) {
-                System.out.println("Introduzca el codigo del componente que desea retirar");
+                System.out.println("Introduzca el código del componente que desea retirar");
                 code = scan.nextInt();
                 if(!miCarrito.eliminarProducto(code)){
-                    System.out.println("\n\tNo hay ningun objeto con ese codigo");
-                };
-
+                    System.out.println("\n\tNo hay ningún objeto con ese código");
+                }
+                
             } else if (decide.equals("NO") == true || decide.equals("no") == true) {
-                System.out.println("\n\nCesta de compra actual, usted tiene:\n" + miCarrito.toString());
+                System.out.println("\n\nCesta de compra actual. Usted tiene:\n" + miCarrito.toString());
                 seguir = false;
             }
         }
@@ -60,20 +57,20 @@ public class TestTienda {
         Scanner scan = new Scanner(System.in);
         Venta tiquetVenta;
 
-        System.out.println("--------------Decida el Método de pago que quiere usar-------------- \n"+ "Con tarjeta o sin Tarjeta(1/0):");
+        System.out.println("--------------Decida el método de pago que quiere usar-------------- \n"+ "Con tarjeta o por transferencia bancaria(0/1):");
         decide= scan.nextInt();
 
         if(decide == 1){
             tiquetVenta = miCarrito.comprar(true); 
             System.out.println(tiquetVenta.toString());
-        }else{
+        } else {
             tiquetVenta = miCarrito.comprar(false); 
             System.out.println(tiquetVenta.toString());
-        };
-
+        }
     }
+    
     public static void main(String[] args) {
-         Scanner scan = new Scanner(System.in);
+    	Scanner scan = new Scanner(System.in);
         System.out.println("-----------------Bienvenido a la tienda de informatica--------------");
 
         Fabricante amd = new Fabricante("AMD", 1000, 609344560);
@@ -83,7 +80,14 @@ public class TestTienda {
         Componente[] tienda = { new Componente(amd, 1000, "Procesador AMD RAEDON", 320.0),
                 new Componente(intel, 1001, "Procesador Intel Core i7", 387.78),
                 new Componente(intel, 1002, "Módulo informatico sistema Intel S9256WK1HLC", 100.0),
-                new Componente(apple, 1003, "Mack book pro", 0.999), new Componente(apple, 1004, "iPad Pro", 0.699), };
+                new Componente(apple, 1003, "Mack book pro", 999.0), 
+                new Componente(apple, 1004, "iPad Pro", 699.0), };
+        
+        System.out.println("Productos:\n");
+        
+        for(int i = 0; i < tienda.length; i++) {
+        	System.out.println(tienda[i].toString() + "\n");
+        }
 
         Cliente sergio = new Cliente(01234, "Sergio", "Nieto", "4502 2304 768");
         Cliente marc = new Cliente(56789, "Marc", "Torrecilla", "3244 5356 2345");
@@ -96,17 +100,14 @@ public class TestTienda {
         while(!comprar ){
         seguirComprando(miCarrito, tienda);
         retirarProducto(miCarrito);
-        System.out.println("Quiere proceder al Pago? SI/NO");
+        System.out.println("Quiere proceder al pago? si/no");
         decide = scan.next();
         if (decide.equals("SI") == true || decide.equals("si") == true) {
             comprar=true;
-        }else if (decide.equals("NO" ) == true || decide.equals("no") == true){
+        } else if (decide.equals("NO" ) == true || decide.equals("no") == true){
             comprar=false;
           }
         }
        pagarCompra(miCarrito);
-      
-
-
     }
 }
