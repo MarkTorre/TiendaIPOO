9037be75b71c83a240ebee9bb00f5a0264e41c71
Serysev <62380919+Serysev@users.noreply.github.com>
2020-04-13 23:54:14 +0200

    Acabado

--- ProductoEnStock.java
+++ ProductoEnStock.java
@@ -1,21 +1,39 @@
-
-public class ProductoEnStock { 
+public class ProductoEnStock {
     private Componente componente;
-    private int disponible, reservado; 
+    private int udsDisponibles, udsReservadas; 
 
-    public ProductoEnStock(Componente componente, int disponible){
-        
-            this.componente=componente;  //no creamos un nuevo componente porque queremos solo la referencia
-            this.disponible=disponible;
-            this.reservado=0;
+    
+    public ProductoEnStock(Componente componente, int udsDisponibles, int udsReservadas){
+            this.componente = componente;  //no creamos un nuevo componente porque queremos solo la referencia
+            this.udsDisponibles = udsDisponibles;
+            this.udsReservadas = udsReservadas;
+    }
+    
+    public ProductoEnStock(Componente componente, int udsDisponibles){
+        this.componente = componente;  //no creamos un nuevo componente porque queremos solo la referencia
+        this.udsDisponibles = udsDisponibles;
+        this.udsReservadas = 0;
     }
 
-public Componente getComponente(){
-    return this.componente;
-}  
 
-public int getDisponible(){
-    return this.disponible;
-}
+    public Componente getComponente(){
+    	return this.componente;
+    }  
 
+	public int getUdsDisponibles() {
+		return udsDisponibles;
+	}
+
+	public void setUdsDisponibles(int udsDisponibles) {
+		this.udsDisponibles = udsDisponibles;
+	}
+
+	public int getUdsReservadas() {
+		return udsReservadas;
+	}
+
+	public void setUdsReservadas(int udsReservadas) {
+		this.udsReservadas = udsReservadas;
+	}
+	
 }
