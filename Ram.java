b9fff7add2082ec7d4e1868ece23d465ec38b728
Serysev <62380919+Serysev@users.noreply.github.com>
2020-04-15 17:06:21 +0200

    getters y setters de String tipo añadidos

--- Ram.java
+++ Ram.java
@@ -1,13 +1,22 @@
 
 public class Ram extends Memoria {
     String tipo;
+    
     public Ram(Fabricante fabricante, int codigo, String descripcion, double pvp, int tamaño, String tipo){
         super(fabricante, codigo, descripcion, pvp, tamaño);
         this.tipo = tipo; 
     }
 
     public String toString(){
-        return  super.toString()+this.tipo+"\n" ;
+        return  super.toString() + "Tipo: " + this.tipo + "\n" ;
+    }
+    
+    public String getTipo(){
+      return this.tipo;
+    }
+    
+    public void setTipo(int tipo) {
+      this.tipo = tipo;
     }
 
 }
