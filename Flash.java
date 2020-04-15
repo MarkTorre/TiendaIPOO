c7bcacde2e8846e12c89b10bfc65f4b9496255e8
Serysev <62380919+Serysev@users.noreply.github.com>
2020-04-15 16:10:48 +0200

    cambio de variable, String tipo por int velocidad

--- Flash.java
+++ Flash.java
@@ -1,14 +1,14 @@
 
 public class Flash extends Memoria{
-    String tipo;
+    int velocidad;
 
-    public Flash(Fabricante fabricante, int codigo, String descripcion, double pvp, int tamaño, String tipo){
+    public Flash(Fabricante fabricante, int codigo, String descripcion, double pvp, int tamaño, int velocidad){
         super(fabricante, codigo, descripcion, pvp, tamaño);
-        this.tipo=tipo;
+        this.velocidad=velocidad;
     }
 
     public String toString(){
-        return  super.toString()+this.tipo+"\n" ;
+        return  super.toString() + this.velocidad+"\n" ;
     }
 
 }
