6f8af125c6c55e622549211f2d50b3080ee34abe
Serysev <62380919+Serysev@users.noreply.github.com>
2020-04-15 17:09:26 +0200

    Clase actualizada

--- Raton.java
+++ Raton.java
@@ -1,4 +1,21 @@
 
 public class Raton {
-
+  int botones;
+  
+  public Raton(Fabricante fabricante, int codigo, String descripcion, double pvp, String puerto, int botones){
+        super(fabricante, codigo, descripcion, pvp, puerto);
+        this.botones = botones; 
+    }
+  
+  public String toString(){
+        return  super.toString() + "Botones: " + this.botones + "\n" ;
+  }
+  
+  public String getBotones(){
+      return this.botones;
+    }
+    
+    public void setBotones(int botones) {
+      this.botones = botones;
+    }
 }
