15eab4cc1693fa6c5edfe071b232dd742d0f0346
Serysev <62380919+Serysev@users.noreply.github.com>
2020-04-15 16:16:08 +0200

    variables añadidas + toString actualizado

--- PlacaBase.java
+++ PlacaBase.java
@@ -1,10 +1,17 @@
 class PlacaBase extends Componente{
-    public PlacaBase(Fabricante fabricante, int codigo, String descripcion, double pvp, int tamaño){
+    String procesador, ranura;
+    int ancho, largo;
+    
+    public PlacaBase(Fabricante fabricante, int codigo, String descripcion, double pvp, int tamaño String procesador, String ranura, int ancho, int largo){
         super(fabricante, codigo, descripcion, pvp);
+        this.procesador = procesador;
+        this.ranura = ranura;
+        this.ancho = ancho;
+        this.largo = largo;
     }
 
     public String toString(){
-        return super.toString();
+        return super.toString() + "Procesador: " + this.procesador + "\n" + "Ancho: " + this.ancho + "\n" + "Largo: " + this.largo + "\n" + "Ranura: " + this.ranura + "\n";
     }
     
 }
