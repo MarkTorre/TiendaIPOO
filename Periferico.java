784503347ec650292059f0f3ed6ebd104251cdf4
Serysev <62380919+Serysev@users.noreply.github.com>
2020-04-15 17:02:25 +0200

    String puerto añadida

--- Periferico.java
+++ Periferico.java
@@ -1,4 +1,21 @@
 
 public class Periferico {
+  private String puerto;
+  
+  public Periferico(Fabricante fabricante, int codigo, String descripcion, double pvp, String puerto){
+    super(fabricante, codigo, descripcion, pvp);
+    this.puerto = puerto;
+  }
+  
+     public String toString(){
+       return  super.toString() + "Puerto de comunicacion: " + this.puerto + "\n" ;
+   }
 
+    public String getPuerto(){
+      return this.puerto;
+    }
+    
+    public void setPuerto(int puerto) {
+      this.puerto = puerto;
+	}
 }
