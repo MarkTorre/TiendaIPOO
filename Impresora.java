bfc716b3a0857dd5292d13c56de459f96645f0f7
Serysev <62380919+Serysev@users.noreply.github.com>
2020-04-15 17:11:15 +0200

    Clase actualizada

---
+++ Impresora.java
@@ -0,0 +1,22 @@
+
+public class Impresora {
+  String impresion;
+  
+  public Impresora(Fabricante fabricante, int codigo, String descripcion, double pvp, String puerto, String impresion){
+        super(fabricante, codigo, descripcion, pvp, puerto);
+        this.impresion = impresion; 
+    }
+  
+  public String toString(){
+        return  super.toString() + "Impresion: " + this.impresion + "\n" ;
+  }
+  
+  public String getImpresion(){
+      return this.impresion;
+    }
+    
+    public void setImpresion(int impresion) {
+      this.impresion = impresion;
+    }
+  
+}
